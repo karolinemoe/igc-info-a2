79c6fa97892c871a15f6d24a5971497b629c17a9
karoline <karolmar@stud.ntnu.no>
2018-10-27 17:20:38 +0200

    api-handler

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 	"encoding/json"
 	"google.golang.org/appengine"
+	"time"
 )
+
+var startTime = time.Now()
 
 func main() {
 
@@ -24,12 +27,34 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	info := &Info{
-		//Uptime: uptime(),
-		Info: "Service for IGC tracks.",
+		Uptime: uptime(),
+		Info: "Service for Paragliding tracks.",
 		Version: "v1",
 	}
 
 	i, _ := json.MarshalIndent(info, "", " ")
 
 	fmt.Fprint(w, string(i))
+}
+
+// calculates time since boot un UNIX time and converts it into a Time object, afterwards converst it to a final string with all components
+func uptime() string{
+
+	uptime := time.Now().Unix() - startTime.Unix()
+	ut := time.Unix(uptime, 0)
+
+	years, months, days 	:= ut.Date()
+	hours, minutes, seconds := ut.Clock()
+	months, days = months-1, days-1
+
+	return fmt.Sprintf("%s%d%s%d%s%d%s%d%s%d%s%d%s", "P", absInt(int64(years - 1970)), "Y", months, "M", days, "DT", hours, "H", minutes, "M", seconds, "S")
+}
+
+// absolute value of n
+func absInt(n int64) int64 {
+
+	if n < 0 {
+		return -n
+	}
+	return n
 }
