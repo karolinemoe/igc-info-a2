216b19b25cc799708dded2a2981918fb9e647f28
karoline <karolmar@stud.ntnu.no>
2018-10-27 17:15:42 +0200

    test

--- main.go
+++ main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
+	"google.golang.org/appengine"
 )
 
 func main() {
 
 	root := "/paragliding"
 	http.HandleFunc(root+"/api", apiHandler)
+
+	appengine.Main()
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello Application")
+
+	type Info struct {
+		Uptime string `json:"uptime"`
+		Info string `json:"info"`
+		Version string `json:"version"`
+	}
+
+	info := &Info{
+		//Uptime: uptime(),
+		Info: "Service for IGC tracks.",
+		Version: "v1",
+	}
+
+	i, _ := json.MarshalIndent(info, "", " ")
+
+	fmt.Fprint(w, string(i))
 }
