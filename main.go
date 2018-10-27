63db945d13ee7da9ce0116c3dbab6a4bf01a237c
karoline <karolmar@stud.ntnu.no>
2018-10-27 17:00:33 +0200

    heroku deploy

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello Assignment 2")
+
+	root := "paragliding"
+	http.HandleFunc(root+"/api", apiHandler)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello Application")
 }
