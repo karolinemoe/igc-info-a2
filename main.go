3ac0054e2b9f220033be0a5855dc82f14b9f244c
karoline <karolmar@stud.ntnu.no>
2018-10-29 12:57:24 +0100

    http redirect from /paragliding to /paragliding/api

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 }
 
 func rootHandler (w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/paragliding/api", 200)
+	http.Redirect(w, r, "/paragliding/api", 301)
 }
 
 /**
