2cac5d4898f7f305d856856d10da267ad14b1d10
karoline <karolmar@stud.ntnu.no>
2018-10-29 16:55:00 +0100

    fix

--- track.go
+++ track.go
@@ -1,9 +1,7 @@
 package main
 
-
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/marni/goigc"
 	"github.com/mitchellh/hashstructure"
 	"net/http"
@@ -51,8 +49,6 @@ func TrackHandler(w http.ResponseWriter, r *http.Request) {
 		if newID < 0 {
 			http.Error(w, "Not able to process the URL", http.StatusBadRequest)
 			return
-		} else if newID == -1 {
-			fmt.Println("Track already exists in database")
 		}
 	default:
 		http.Error(w, "No specified request method", 400); return
